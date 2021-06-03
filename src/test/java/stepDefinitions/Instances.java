7bd50d516c0e46505f02263ab6296fe9be3d1115
Vasyl <quartblabla@gmail.com>
2021-06-03 14:17:51 +0300

    first commit

---
+++ src/test/java/stepDefinitions/Instances.java
@@ -0,0 +1,17 @@
+package stepDefinitions;
+
+import org.openqa.selenium.WebDriver;
+
+
+
+
+
+
+public class Instances {
+    protected static WebDriver driver;
+    protected static webPages.HomePage homePage;
+    protected static webPages.LoginPage loginPage;   
+    protected static webPages.CreateEventPage createEventPage;
+    protected static webPages.EditEventDetailsPage editEventDetailsPage;
+
+}
